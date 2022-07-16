e56a80ecff79324509748ae7493b54a08931178e
Robby19 <rizkyrobby19@gmail.com>
2022-07-16 07:41:41 +0700

    initial dom logic

--- index.js
+++ index.js
@@ -5,13 +5,41 @@ let getComSelection = () => {
     if (randomNumber > 0.33 && randomNumber <= 0.67) return "kertas";
     return "gunting";
 };
-let gameResult = (playerSelection, getComSelection) => {
+let gameRules = (playerSelection, comSelection) => {
     // set the game rules
-    if (playerSelection == getComSelection) return "seri";
+    if (playerSelection == comSelection) return "seri";
     if (playerSelection == "batu")
-        return getComSelection == "gunting" ? "player win" : "com win";
+        return comSelection == "gunting" ? "player win" : "com win";
     if (playerSelection == "kertas")
-        return getComSelection == "batu" ? "player win" : "com win";
+        return comSelection == "batu" ? "player win" : "com win";
     if (playerSelection == "gunting")
-        return getComSelection == "kertas" ? "player win" : "com win";
+        return comSelection == "kertas" ? "player win" : "com win";
 };
+
+let playerSelectionBatu = document.querySelector("#batu");
+playerSelectionBatu.addEventListener("click", () => {
+    alert("oke");
+    const comSelection = getComSelection();
+    const playerSelection = playerSelectionBatu.id;
+    const result = gameRules(playerSelection, comSelection);
+    // select player active
+    playerSelectionBatu.classList.add("active");
+    // select com active
+    let comSelect = document.querySelector("#com-" + comSelection);
+    comSelect.classList.add("active");
+    // select result
+    let gameResult = document.querySelector("#result");
+    gameResult.classList.add("active");
+    let textResult = document.querySelector(".hasil");
+    textResult.innerHTML = result;
+    textResult.classList.replace("vs", "game-result");
+    // reset button
+    let reset = document.querySelector("#reset");
+    reset.addEventListener("click", () => {
+        playerSelectionBatu.classList.remove("active");
+        comSelect.classList.remove("active");
+        gameResult.classList.remove("active");
+        textResult.innerHTML = "vs";
+        textResult.classList.replace("game-result", "vs");
+    });
+});
